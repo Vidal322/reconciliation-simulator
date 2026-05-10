a98f87e6ab8d3fe10d5f66551606af3884e23f6a
CBaquero <cbm@di.uminho.pt>
2026-05-10 18:39:10 +0100

    exp22: tree fpr 0.27->0.28

--- src/simulator/protocols/multi_replica.rs
+++ src/simulator/protocols/multi_replica.rs
@@ -41,7 +41,7 @@ impl MultiReplicaProtocol {
         match kind {
             TopologyKind::Star => 0.01,
             TopologyKind::Chord => 0.1,
-            TopologyKind::Tree => 0.27,
+            TopologyKind::Tree => 0.28,
         }
     }
 
